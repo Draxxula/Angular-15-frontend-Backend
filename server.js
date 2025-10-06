ad8a8e66cb4e4e1c9a63ea7976d60a8c677c1906
Draxxula <angcahandjx123@gamil.com>
2025-10-06 22:10:34 +0800

    updated to test deployment

--- server.js
+++ server.js
@@ -12,7 +12,9 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 // allow cors requests from any origin and with credentials
-app.use(cors({ origin: (origin, callback) => callback(null, true), credentials: true }));
+//app.use(cors({ origin: (origin, callback) => callback(null, true), credentials: true }));
+
+app.use(cors({ origin: 'https://websystemtest.vercel.app' }));
 
 // api routes
 //accounts routes
